9d1956b7f1cf389bd9c065510fd688b6fd979243
Credgroup <54108885+Credgroup@users.noreply.github.com>
2025-07-01 16:43:30 -0300

    add mock

--- src/pages/FormsPage/useFormPageHook.ts
+++ src/pages/FormsPage/useFormPageHook.ts
@@ -5,13 +5,13 @@ import { toast } from "sonner";
 import { execApi } from "@/hooks/useApi";
 import { useIdProposalGroupStore } from "@/stores/useIdProposalGroup";
 import { useMutation } from "@tanstack/react-query";
-import { useLayoutStore } from "@/stores/useLayoutStore";
+// import { useLayoutStore } from "@/stores/useLayoutStore";
 import { dev_log } from "@/lib/utils";
-// import { mockData } from "./mock";
+import { mockData } from "./mock";
 
 export const useFormPageHook = () => {
-  const layoutObj = useLayoutStore((state) => state.layoutObject);
-  // const layoutObj = mockData;
+  // const layoutObj = useLayoutStore((state) => state.layoutObject);
+  const layoutObj = mockData;
   const navigate = useNavigate();
   const [sidebar, setSidebar] = useState<Partial<SessaoType>[] | null>(null);
   const [currentSessao, setCurrentSessao] =
