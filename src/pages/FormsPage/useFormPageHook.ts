398e5015da36e0eaf5af3255edb024f2f719a6ab
Alisson Lima <renalima.rl46@gmail.com>
2025-07-03 16:44:33 -0300

    fix build errors

--- src/pages/FormsPage/useFormPageHook.ts
+++ src/pages/FormsPage/useFormPageHook.ts
@@ -7,7 +7,7 @@ import { useIdProposalGroupStore } from "@/stores/useIdProposalGroup";
 import { useMutation } from "@tanstack/react-query";
 import { useLayoutStore } from "@/stores/useLayoutStore";
 import { dev_log } from "@/lib/utils";
-import { mockData } from "./mock";
+// import { mockData } from "./mock";
 
 export const useFormPageHook = () => {
   const layoutObj = useLayoutStore((state) => state.layoutObject);
