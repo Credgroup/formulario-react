6206479c801731b534332e5d1b033b26bedb1cc8
Alisson Lima <tecnologia@credgroup.com.br>
2026-06-02 16:09:50 -0300

    Update useInspectionLayout.tsx

--- src/hooks/useInspectionLayout.tsx
+++ src/hooks/useInspectionLayout.tsx
@@ -13,47 +13,14 @@ export default function useInspectionLayout() {
           data: {},
           method: "GET",
         });
+        console.log(res)
         if (!res || !res.data) {
           throw new Error("Aconteceu algum erro ao buscar o layout da inspeção");
         }
         return res.data;
       } catch (error: unknown) {
         dev_log(() => console.log(error));
-        return {
-          "idInspecao": Number(idInspecao) || 1,
-          "dsLocal": "Galpão 1 - Atz",
-          "enderecoCompleto": "av Teste, 1800, Sao Paulo-SP",
-          "grupos": [
-            {
-              "cdPrioridade": 3,
-              "descricaoPrioridade": "Alta",
-              "recomendacoes": [
-                {
-                  "idRecomendacao": 19,
-                  "dsRecomendacao": "Instalar Alarmes",
-                  "dsMotivacao": "sinalização",
-                  "cdCategoria": 0,
-                  "cdImpacto": 1,
-                  "exigeEvidencia": false
-                }
-              ]
-            },
-            {
-              "cdPrioridade": 20986,
-              "descricaoPrioridade": "Geral",
-              "recomendacoes": [
-                {
-                  "idRecomendacao": 1,
-                  "dsRecomendacao": "Instalar sprinklers na área X",
-                  "dsMotivacao": "striPor que isso é um risco? (Padrão ouro em laudos)ng",
-                  "cdCategoria": 0,
-                  "cdImpacto": 1,
-                  "exigeEvidencia": false
-                }
-              ]
-            }
-          ]
-        };
+        throw error
       }
     },
   });
